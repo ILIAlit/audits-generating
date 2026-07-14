b23bac06346a0a07ea6c5d789fb017de6f3ff5da
ILIAlit <lila96611@gmail.com>
2026-07-14 22:02:44 +0300

    Fix: The package cache has been redirected to /tmp and added.gitkeep

--- api/index.php
+++ api/index.php
@@ -1,22 +1,37 @@
 <?php
 
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
 // Регистрируем автозагрузчик Composer
 require __DIR__ . '/../vendor/autoload.php';
 
-// Запускаем приложение Laravel
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+// Перенаправляем ВСЕ кэш-файлы Laravel во временную папку /tmp, доступную на запись
+$cachePath = '/tmp/bootstrap-cache';
+if (!is_dir($cachePath)) {
+    mkdir($cachePath, 0755, true);
+}
 
-// Перенаправляем скомпилированные views в /tmp
+putenv("APP_packages_CACHE={$cachePath}/packages.php");
+putenv("APP_services_CACHE={$cachePath}/services.php");
+putenv("APP_config_CACHE={$cachePath}/config.php");
+putenv("APP_routes_CACHE={$cachePath}/routes.php");
+
 if (!is_dir('/tmp/views')) {
     mkdir('/tmp/views', 0755, true);
 }
 putenv('VIEW_COMPILED_PATH=/tmp/views');
 
+// Запускаем приложение
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
 // Обрабатываем входящий запрос
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+    $request = Request::capture()
 );
 
 $response->send();
